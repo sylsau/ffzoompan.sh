0b1e539844961581e74fdf8c35df860b1722ddac
Sylvain Saubier <mail@sylsau.com>
2023-03-07 19:40:17 +0100

    framerate option

--- ffzoompan.sh
+++ ffzoompan.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 [[ $DEBUG ]] && set -o nounset
 set -o pipefail -o errexit -o errtrace
 trap 'echo -e "${FMT_BOLD}ERROR${FMT_OFF}: at $FUNCNAME:$LINENO"' ERR
@@ -10,6 +11,7 @@ FFMPEG="${FFMPEG:-ffmpeg}"
 INFILE=
 OUTFILE=/tmp/ffzoompan-out.mp4
 DURATION=20
+FRAMERATE=24
 CRF=12
 ZOOMMAX=2
 OUTSIZE="1080x1920"
@@ -36,19 +38,20 @@ fn_say() {
 
 fn_help() {
     cat << EOF
-$PROGNAME v20201217
+$PROGNAME v20230303
     Takes an image or a video and creates a video with a zoom effect into the center of the picture.
 
 REQUIREMENTS
     ffmpeg, file, bc
 
 USAGE
-    $PROGNAME -i INPUT_FILE [-o OUTPUT_FILE -t DURATION -q CRF -z MAX_ZOOM -s SIZE -a FF_OPTS]
+    $PROGNAME -i INPUT_FILE [-o OUTPUT_FILE -t DURATION -r FRAMERATE -q CRF -z MAX_ZOOM -s SIZE -a FF_OPTS]
 
 OPTIONS AND ARGUMENTS
     INPUT_FILE      path to input file
     OUTPUT_FILE     path to output file [default: $OUTFILE]
     DURATION        duration of the output video (in seconds) [default: $DURATION]
+    FRAMERATE       framerate of the output video (fps) [default: $FRAMERATE]
     CRF             quality of the output video (constant rate factor) [default: $CRF]
     MAX_ZOOM        zoom value at the end of output video (eg. 2 => x2) [default: $ZOOMMAX]
     SIZE            size of output video (WIDTHxHEIGHT) [default: $OUTSIZE]
@@ -94,6 +97,10 @@ else
                 CRF="$2"
                 shift
                 ;;
+            "-r")
+                FRAMERATE="$2"
+                shift
+                ;;
             "-t")
                 DURATION="$2"
                 shift
@@ -135,11 +142,11 @@ fn_is_img "$INFILE"
 if [[ "$RES" = "1" ]]; then
     $FFMPEG -framerate 25 -loop 1 -i "$INFILE" -vf "scale=(iw*$ZOOMMAX*2):(ih*$ZOOMMAX*2), 
         zoompan=z='min(pzoom+($ZOOMMAX-1)/$LENGTH,$ZOOMMAX)':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':fps=25:s=$OUTSIZE" \
-        -t $DURATION -crf:v $CRF $FF_OPTS -y "$OUTFILE"
+        -t $DURATION -r $FRAMERATE -crf:v $CRF $FF_OPTS -y "$OUTFILE"
 else
     $FFMPEG                       -i "$INFILE" -vf "scale=(iw*$ZOOMMAX*2):(ih*$ZOOMMAX*2), 
         zoompan=z='min(pzoom+($ZOOMMAX-1)/$LENGTH,$ZOOMMAX)':d=1:x='iw/2-(iw/zoom/2)':y='ih/2-(ih/zoom/2)':fps=25:s=$OUTSIZE"  \
-        -t $DURATION -crf:v $CRF -c:a copy $FF_OPTS -y "$OUTFILE"
+        -t $DURATION -r $FRAMERATE -crf:v $CRF -c:a copy $FF_OPTS -y "$OUTFILE"
 fi
 
 fn_say "output to $OUTFILE"
